00b0eb0b76ce73280a71ef1a9cd9f000ad4c4d63
Zurma <12ab@gmx.fr>
2017-11-22 23:35:03 +0100

    Structure API

--- bluetooth_api.py
+++ bluetooth_api.py
@@ -4,10 +4,14 @@ from flask_sqlalchemy import *
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'thisissecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/c/Users/antho/Documents/api_example/todo.db'
+app.config['SECRET_KEY'] = '17161a92-9c2c-4795-93ab-b3a860391d50'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@server/db'
 
 db = SQLAlchemy(app)
+
+"""
+Tables
+"""
 
 class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
@@ -54,9 +58,77 @@ class Detection(db.Model):
     datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     adresse_mac = db.Column(db.String(20), nullable=False)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+"""
+API Client
+"""
+
+@app.route('/client', methods=['POST'])
+def create_client():
+    return ''
+
+@app.route('/client', methods=['GET'])
+def view_clients():
+    return ''
+
+@app.route('/client/<id>', methods=['PUT'])
+def edit_client(id):
+    return ''
+
+@app.route('/client/<id>', methods=['DELETE'])
+def delete_client(id):
+    return ''
+
+@app.route('/client/<id>', methods=['GET'])
+def view_client(id):
+    return ''
+
+"""
+API Visite
+"""
+
+@app.route('/visite', methods=['POST'])
+def create_visite():
+    return ''
+
+@app.route('/visite', methods=['GET'])
+def view_visites():
+    return ''
+
+"""
+API Promotion
+"""
+
+@app.route('/promotion', methods=['POST'])
+def create_promotion():
+    return ''
+
+@app.route('/promotion', methods=['GET'])
+def view_promotions():
+    return ''
+
+@app.route('/promotion/<id>', methods=['PUT'])
+def edit_promotion(id):
+    return ''
+
+@app.route('/promotion/<id>', methods=['DELETE'])
+def delete_promotion(id):
+    return ''
+
+@app.route('/promotion/<id>', methods=['GET'])
+def view_promotion(id):
+    return ''
+
+"""
+API Categorie
+"""
+
+@app.route('/categorie', methods=['POST'])
+def create_categorie():
+    return ''
+
+@app.route('/categorie', methods=['GET'])
+def view_categories():
+    return ''
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
