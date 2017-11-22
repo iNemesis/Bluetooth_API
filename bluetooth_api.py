e64517e88bd50e49dbb21e72e1106b96646b9ff3
Zurma <12ab@gmx.fr>
2017-11-22 23:01:36 +0100

    Creation des tables

--- bluetooth_api.py
+++ bluetooth_api.py
@@ -24,17 +24,35 @@ class Client(db.Model):
     date_creation = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     visites = db.relationship('Visite', backref='client', lazy='dynamic')
+    promotions = db.relationship('Promotion', secondary='promotions',
+                                 backref=db.backref('clients', lazy=True),
+                                 lazy='subquery')
+
+promotions = db.Table('promotions',
+    db.Column('client_id', db.Integer, db.ForeignKey('client.id'), primary_key=True),
+    db.Column('promotion_id', db.Integer, db.ForeignKey('promotion.id'), primary_key=True)
+)
+
+class Promotion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.Text, nullable=False)
+
+    client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
 
 class Visite(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-    client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
+class Categorie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(30), nullable=False)
 
+    promotions = db.relationship('Promotion', backref='categorie', lazy='dynamic')
 
-
-
-
+class Detection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    adresse_mac = db.Column(db.String(20), nullable=False)
 
 @app.route('/')
 def hello_world():
