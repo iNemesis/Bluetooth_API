db49e6a2e075f685fd83d16ecc4afb949bf63ca5
Zurma <12ab@gmx.fr>
2017-12-08 15:15:11 +0100

    Correction bug db + creation/modification de l'api

--- bluetooth_api.py
+++ bluetooth_api.py
@@ -9,7 +9,7 @@ Configuration
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '17161a92-9c2c-4795-93ab-b3a860391d50'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@server/db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1:3306/db'
 
 db = SQLAlchemy(app)
 
@@ -36,6 +36,21 @@ class Client(db.Model):
                                  backref=db.backref('clients', lazy=True),
                                  lazy='subquery')
 
+    def __init__(self, nom, prenom, telephone, rue, ville, code_postal, pays, adresse_mac, date_naissance, email, date_creation=None):
+        self.nom = nom
+        self.prenom = prenom
+        self.telephone = telephone
+        self.rue = rue
+        self.ville = ville
+        self.code_postal = code_postal
+        self.pays = pays
+        self.adresse_mac = adresse_mac
+        self.date_naissance = date_naissance
+        self.email = email
+        if date_creation != None:
+            self.date_creation = date_creation
+
+
 promotions = db.Table('promotions',
     db.Column('client_id', db.Integer, db.ForeignKey('client.id'), primary_key=True),
     db.Column('promotion_id', db.Integer, db.ForeignKey('promotion.id'), primary_key=True)
@@ -45,11 +60,22 @@ class Promotion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.Text, nullable=False)
 
+    categorie_id = db.Column(db.Integer, db.ForeignKey('categorie.id'))
     client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
+
+    def __init__(self, message, categorie_id, client_id):
+        self.message = message
+        self.categorie_id = categorie_id
+        self.client_id = client_id
 
 class Visite(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    
+    client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
+
+    def __init__(self, client_id):
+        self.client_id = client_id
 
 class Categorie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -57,10 +83,16 @@ class Categorie(db.Model):
 
     promotions = db.relationship('Promotion', backref='categorie', lazy='dynamic')
 
+    def __init__(self, nom):
+        self.nom = nom
+
 class Detection(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     adresse_mac = db.Column(db.String(20), nullable=False)
+
+    def __init__(self, adresse_mac):
+        self.adresse_mac = adresse_mac
 
 """
 API Client
@@ -68,15 +100,37 @@ API Client
 
 @app.route('/client', methods=['POST'])
 def create_client():
-    return ''
+    data = request.get_json()
+
+    new_client = Client(data['nom'], data['prenom'], data['telephone'], data['rue'], data['ville'], data['code_postal'],
+                        data['pays'], data['adresse_mac'], data['date_naissance'], data['email'])
+    db.session.add(new_client)
+    db.session.commit()
+
+    return jsonify({'message': 'New user created!'})
 
 @app.route('/client', methods=['GET'])
 def view_clients():
-    return ''
+    clients = Client.query.all()
+    output = []
 
-@app.route('/client/<id>', methods=['PUT'])
-def edit_client(id):
-    return ''
+    for client in clients:
+        client_data = {}
+        client_data['id'] = client.id
+        client_data['nom'] = client.nom
+        client_data['prenom'] = client.prenom
+        client_data['telephone'] = client.telephone
+        client_data['rue'] = client.rue
+        client_data['ville'] = client.ville
+        client_data['code_postal'] = client.code_postal
+        client_data['pays'] = client.pays
+        client_data['adresse_mac'] = client.adresse_mac
+        client_data['date_naissance'] = client.date_naissance
+        client_data['email'] = client.email
+        client_data['date_creation'] = client.date_creation
+        output.append(client_data)
+
+    return jsonify({'clients' : output})
 
 @app.route('/client/<id>', methods=['DELETE'])
 def delete_client(id):
@@ -84,7 +138,26 @@ def delete_client(id):
 
 @app.route('/client/<id>', methods=['GET'])
 def view_client(id):
-    return ''
+    client = Client.query.filter_by(id=id).first()
+
+    if not client:
+        return jsonify({'message': 'No user found!'})
+
+    client_data = {}
+    client_data['id'] = client.id
+    client_data['nom'] = client.nom
+    client_data['prenom'] = client.prenom
+    client_data['telephone'] = client.telephone
+    client_data['rue'] = client.rue
+    client_data['ville'] = client.ville
+    client_data['code_postal'] = client.code_postal
+    client_data['pays'] = client.pays
+    client_data['adresse_mac'] = client.adresse_mac
+    client_data['date_naissance'] = client.date_naissance
+    client_data['email'] = client.email
+    client_data['date_creation'] = client.date_creation
+
+    return jsonify({'client': client_data})
 
 """
 API Visite
@@ -108,10 +181,6 @@ def create_promotion():
 
 @app.route('/promotion', methods=['GET'])
 def view_promotions():
-    return ''
-
-@app.route('/promotion/<id>', methods=['PUT'])
-def edit_promotion(id):
     return ''
 
 @app.route('/promotion/<id>', methods=['DELETE'])
@@ -139,4 +208,8 @@ Run
 """
 
 if __name__ == '__main__':
+    try:
+        db.create_all()
+    except:
+        print("Database non creee")
     app.run(debug=True)
